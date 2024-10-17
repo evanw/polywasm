f99f1bafd38b7a41031d331c1b422f5ca782dc35
Evan Wallace <evan.exe@gmail.com>
2024-10-17 12:09:02 -0400

    add some additional top-level api methods

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,22 @@
 import { Global, Instance, Memory, Table } from './instantiate'
 import { Module, CompileError } from './parse'
 
-const instantiate = async (input: any, importObject?: WebAssembly.Imports): Promise<any> => {
+const compile = async (input: BufferSource): Promise<Module> => {
+  return new Module(input)
+}
+
+const compileStreaming = async (source: Response | PromiseLike<Response>): Promise<Module> => {
+  return new Module(await (await source).arrayBuffer())
+}
+
+const instantiate = async (input: BufferSource | Module, importObject?: WebAssembly.Imports): Promise<any> => {
   if (input instanceof Module) return new Instance(input, importObject)
   const module = new Module(input)
+  return { module, instance: new Instance(module, importObject) }
+}
+
+const instantiateStreaming = async (source: Response | PromiseLike<Response>, importObject?: WebAssembly.Imports): Promise<any> => {
+  const module = new Module(await (await source).arrayBuffer())
   return { module, instance: new Instance(module, importObject) }
 }
 
@@ -22,7 +35,10 @@ const validate = (input: any): boolean => {
 const wasmAPI: Partial<typeof WebAssembly> = {
   Global,
   Instance,
+  compile,
+  compileStreaming,
   instantiate,
+  instantiateStreaming,
   validate,
   Memory,
   Module,
